e842b7d70328a38cec6219087f7074b84f5cd6ac
TheTraille18 <jtraille07@gmail.com>
2021-07-22 22:09:18 -0400

    Added http endpoints

--- src/main/java/com/ablackcloudapp/Shopping4ChowApi/ChowController/ChowController.java
+++ src/main/java/com/ablackcloudapp/Shopping4ChowApi/ChowController/ChowController.java
@@ -1,27 +1,48 @@
 package com.ablackcloudapp.Shopping4ChowApi.ChowController;
 
-import com.ablackcloudapp.Shopping4ChowApi.Service.iFoodService;
+import com.ablackcloudapp.Shopping4ChowApi.Entity.Ingredient;
+import com.ablackcloudapp.Shopping4ChowApi.Service.iIngredientService;
+import com.amazonaws.services.s3.AmazonS3;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
-@Controller
+import java.util.List;
+
+@CrossOrigin
+@RestController
 public class ChowController {
 
     @Autowired
-    private iFoodService foodService;
+    private iIngredientService ingredientService;
+
+    @Autowired
+    private AmazonS3 s3client;
 
     @ResponseBody
-    @GetMapping("test")
-    public String test(){
-        return "test";
+    @PostMapping("getIngredients")
+    public List<Ingredient> getingredient(@RequestBody Ingredient ingredient){
+        System.out.println("Search Item: " + ingredient.getName());
+        List<Ingredient> ingredients = ingredientService.getIngredients(ingredient.getName());
+        System.out.println(ingredients);
+        return ingredients;
     }
 
     @ResponseBody
-    @GetMapping("addfood")
-    public String addFood(){
-        System.out.printf("Adding food");
-        return null;
+    @GetMapping("allingredients")
+    public List<Ingredient> getAllIngredients(){
+        List<Ingredient> ingredients = ingredientService.getAllIngredients();
+        return ingredients;
     }
+
+    @ResponseBody
+    @PostMapping("addingredient")
+    public String addFood(@RequestParam("file") MultipartFile foodPic, @RequestPart("name") String name) {
+    //public String addFood(@RequestBody Food food){
+        System.out.println("Adding Ingredient");
+        Ingredient ingredient = new Ingredient(name, foodPic);
+        ingredientService.addIngredient(ingredient);
+        return "Ingredient Added";
+    }
+
 }
